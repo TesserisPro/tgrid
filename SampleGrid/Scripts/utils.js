ca0d2a21d123a2dc7bce1db62b5dff53795ddb9a
proleskovskiy <proleskovskiy@tesseris.com>
2013-09-09 12:35:43 +0300

    Initial state (base grid + sorting)

---
+++ SampleGrid/Scripts/utils.js
@@ -0,0 +1,33 @@
+﻿//utilities
+function isNull(target) {
+    return target == null;
+};
+
+function isNotNull(target) {
+    return target != null;
+};
+
+function isUndefined(target) {
+    return typeof target == "undefined";
+};
+
+function isNotUndefined(target) {
+    return typeof target != "undefined";
+};
+
+function isNoU(target) {
+    return isUndefined(target) && isNull(target);
+};
+
+function isNotNoU(target) {
+    return isNotUndefined(target) && isNotNull(target);
+};
+
+//sort
+function CompareByProperty(a, b, propertyName) {
+    return a[propertyName].toString().localeCompare(b[propertyName]);
+}
+
+function CompareByPropertyValue(a, b, propertyName) {
+    return a[propertyName]().toString().localeCompare(b[propertyName]());
+}
