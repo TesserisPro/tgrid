ba11dc50c98422e203f48c170281a900a93d853d
feafarot <sly.feafarot@gmail.com>
2013-09-12 17:23:54 +0300

    Fixed builed. Cleanuped.

--- MobileGrid/Controllers/HomeController.cs
+++ MobileGrid/Controllers/HomeController.cs
@@ -14,19 +14,5 @@ namespace GridSample.Controllers
 
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
--- MobileGrid/Global.asax.cs
+++ MobileGrid/Global.asax.cs
@@ -22,7 +22,6 @@ namespace GridSample
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
