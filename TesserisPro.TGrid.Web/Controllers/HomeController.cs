505488189b33d5d5ec79228d725e99c3b4a4f362
juliatrikoz <tjulia@tesseris.com>
2014-01-09 16:38:44 +0200

    Task 167 footer template html and js demos

--- TesserisPro.TGrid.Web/Controllers/HomeController.cs
+++ TesserisPro.TGrid.Web/Controllers/HomeController.cs
@@ -233,13 +233,13 @@ namespace TesserisPro.TGrid.Web
             {
                 title = "Footer template",
                 url = "Knockout/FooterTemplateKnockout",
-                htmlUrl = "Knockout/EditingWithCellTemplateHtml",
+                htmlUrl = "Knockout/FooterTemplateKnockoutHtml",
                 cssUrl = "Knockout/StyleCss",
-                jsUrl = "Knockout/jaEditingWithCellTemplateKnockout",
+                jsUrl = "Knockout/jsFooterTemplateKnockout",
                 angularUrl = "Angular/FooterTemplateAngular",
-                angularHtmlUrl = "Angular/EditingWithCellTemplateHtml",
+                angularHtmlUrl = "Angular/FooterTemplateAngularHtml",
                 angularCssUrl = "Angular/StyleCss",
-                angularJsUrl = "Angular/jaEditingWithCellTemplateAngular"
+                angularJsUrl = "Angular/jsFooterTemplateAngular"
             });
 
             List<UIModel> listOfDemosMobile = new List<UIModel>();
